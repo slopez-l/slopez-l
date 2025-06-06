c7d3ca57d3c07a51f0841b85f8ac4dbf71074ad3
slopez-l <sergio_lopezlopez@hotmail.com>
2025-06-06 10:20:38 +0200

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -30,16 +30,14 @@ int	main(void)
 	while (i <= 100)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-			write(1, "fizzbuzz\n", 9);
+			write(1, "fizzbuzz", 8);
 		else if ((i % 3) == 0)
-			write(1, "fizz\n", 5);
+			write(1, "fizz", 4);
 		else if ((i % 5) == 0)
-			write(1, "buzz\n", 5);
+			write(1, "buzz", 4);
 		else
-		{
 			ft_print_nbr(i);
-			write(1, "\n", 1);
-		}
+		write(1, "\n", 1);
 		i++;
 	}
 	return (0);
